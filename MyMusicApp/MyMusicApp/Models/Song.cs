8fc03ce29474b7b4165fd318714b195084baef3f
Darren Ruane <Darzer@users.noreply.github.com>
2016-04-16 01:59:52 +0100

    -Added songs tab and functionality. -Added Display Data Connotations.

---
+++ MyMusicApp/MyMusicApp/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusicApp.Models
+{
+    public class Song
+    {
+        public int ID { get; set; }
+        [Display(Name = "Song")]
+        public string Title { get; set; }
+        public int AlbumID { get; set; }
+        [Display(Name = "Duration")]
+        public TimeSpan TrackLength { get; set; }
+        public virtual Album Album { get; set; }
+    }
+}
